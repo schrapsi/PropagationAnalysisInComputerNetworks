5763d34197df88477d56459903e2ad0c04642618
ucfoh <ucfoh@student.kit.edu>
2022-02-28 16:36:20 +0100

    added Javadoc in Exceptions

--- src/edu/kit/informatik/exceptions/InvalidTreeException.java
+++ src/edu/kit/informatik/exceptions/InvalidTreeException.java
@@ -1,5 +1,9 @@
 package edu.kit.informatik.exceptions;
-
+/**
+ * This class is and exception if the tree can´t be build properly
+ * @author ucfoh
+ * @version 1.0
+ */
 public class InvalidTreeException extends RuntimeException {
 
     public InvalidTreeException(String message) {
--- src/edu/kit/informatik/exceptions/ParseException.java
+++ src/edu/kit/informatik/exceptions/ParseException.java
@@ -1,5 +1,10 @@
 package edu.kit.informatik.exceptions;
 
+/**
+ * This class is and exception for wrong string inputs.
+ * @author ucfoh
+ * @version 1.0
+ */
 public class ParseException extends Exception {
     public ParseException(String message) {
         super(message);
