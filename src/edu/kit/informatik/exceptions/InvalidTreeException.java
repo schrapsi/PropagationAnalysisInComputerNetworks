f37369bddab1880c3afbbaa0583f0344168b9c67
ucfoh <ucfoh@student.kit.edu>
2022-02-28 16:41:07 +0100

    added Javadoc in Exceptions again

--- src/edu/kit/informatik/exceptions/InvalidTreeException.java
+++ src/edu/kit/informatik/exceptions/InvalidTreeException.java
@@ -1,11 +1,17 @@
 package edu.kit.informatik.exceptions;
+
 /**
  * This class is and exception if the tree can´t be build properly
  * @author ucfoh
  * @version 1.0
  */
+
 public class InvalidTreeException extends RuntimeException {
 
+    /**
+     * Constructor for returning a given Message
+     * @param message with the Error
+     */
     public InvalidTreeException(String message) {
         super(message);
     }
--- src/edu/kit/informatik/exceptions/ParseException.java
+++ src/edu/kit/informatik/exceptions/ParseException.java
@@ -6,6 +6,11 @@ package edu.kit.informatik.exceptions;
  * @version 1.0
  */
 public class ParseException extends Exception {
+
+    /**
+     * Constructor for returning a given Message
+     * @param message with the Error
+     */
     public ParseException(String message) {
         super(message);
     }
