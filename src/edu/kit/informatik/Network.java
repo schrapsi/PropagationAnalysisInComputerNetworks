c0f3c759812c1be7a0870b6e3dc889a9b0ef7be6
ucfoh <ucfoh@student.kit.edu>
2022-03-02 19:18:29 +0100

    added more JavaDoc

--- src/edu/kit/informatik/Network.java
+++ src/edu/kit/informatik/Network.java
@@ -1,6 +1,5 @@
 package edu.kit.informatik;
 
-
 import edu.kit.informatik.exceptions.InvalidTreeException;
 import edu.kit.informatik.exceptions.ParseException;
 
@@ -26,7 +25,7 @@ public class Network {
 
     private static final String ILLEGAL_TREE_TOPOLOGY = "Its not allowed to add a circular Graph";
     private static final int SMALLEST_NOTATION_POSSIBLE = 17;
-    private final Map<IP, TreeSet<IP>> treeTopology = new HashMap<>();
+    private final Map<IP, TreeSet<IP>> adjacencyList = new HashMap<>();
 
     /**
      * This constructor creates a new network instance and adds a valid tree topology if the inputs were correct.
@@ -37,10 +36,10 @@ public class Network {
     public Network(final IP root, final List<IP> children) {
 
         TreeSet<IP> childSet = constructorInputValidation(root, children);
-        treeTopology.put(root, childSet);
+        adjacencyList.put(root, childSet);
         for (IP child : childSet) {
-            treeTopology.put(child, new TreeSet<>());
-            treeTopology.get(child).add(root);
+            adjacencyList.put(child, new TreeSet<>());
+            adjacencyList.get(child).add(root);
         }
     }
 
@@ -62,11 +61,11 @@ public class Network {
             if (ip.contains("(")) {
                 if (firstElement) {
                     newIp = new IP(ip.substring(1));
-                    treeTopology.put(newIp, new TreeSet<>());
+                    adjacencyList.put(newIp, new TreeSet<>());
                     firstElement = false;
                 } else {
                     newIp = new IP(ip.substring(1));
-                    convertIPToAdjList(newIp, rootList);
+                    addIP(newIp, rootList);
                 }
                 rootList.add(new IP(ip.substring(1)));
 
@@ -78,14 +77,14 @@ public class Network {
                     }
                 }
                 newIp = new IP(ip.substring(0, ip.length() - counter));
-                convertIPToAdjList(newIp, rootList);
+                addIP(newIp, rootList);
                 for (int i = 0; i < counter; i++) {
                     rootList.removeLast();
                 }
 
             } else {
                 newIp = new IP(ip);
-                convertIPToAdjList(newIp, rootList);
+                addIP(newIp, rootList);
             }
         }
     }
@@ -104,27 +103,27 @@ public class Network {
         }
 
         Map<IP, TreeSet<IP>> topologySave = new HashMap<>();
-        for (IP ip : this.treeTopology.keySet()) {
-            topologySave.put(ip, new TreeSet<>(this.treeTopology.get(ip)));
+        for (IP ip : this.adjacencyList.keySet()) {
+            topologySave.put(ip, new TreeSet<>(this.adjacencyList.get(ip)));
 
         }
 
-        for (IP ip : subnet.treeTopology.keySet()) {
-            if (!this.treeTopology.containsKey(ip)) {
+        for (IP ip : subnet.adjacencyList.keySet()) {
+            if (!this.adjacencyList.containsKey(ip)) {
 
-                this.treeTopology.put(ip, new TreeSet<>());
-                this.treeTopology.get(ip).addAll(subnet.treeTopology.get(ip));
+                this.adjacencyList.put(ip, new TreeSet<>());
+                this.adjacencyList.get(ip).addAll(subnet.adjacencyList.get(ip));
 
             } else {
-                this.treeTopology.get(ip).addAll(subnet.treeTopology.get(ip));
+                this.adjacencyList.get(ip).addAll(subnet.adjacencyList.get(ip));
             }
         }
 
         LinkedHashSet<IP> visited = new LinkedHashSet<>();
-        for (IP ip : this.treeTopology.keySet()) {
+        for (IP ip : this.adjacencyList.keySet()) {
             if (!visited.contains(ip) && checkCycle(ip, null, visited)) {
-                this.treeTopology.clear();
-                this.treeTopology.putAll(topologySave);
+                this.adjacencyList.clear();
+                this.adjacencyList.putAll(topologySave);
                 return false;
             }
         }
@@ -136,8 +135,7 @@ public class Network {
      * @return a list of all nodes existing in the tree topology
      */
     public List<IP> list() {
-        List<IP> list = new ArrayList<>();
-        list.addAll(this.treeTopology.keySet());
+        List<IP> list = new ArrayList<>(this.adjacencyList.keySet());
         Collections.sort(list);
         return list;
     }
@@ -153,11 +151,11 @@ public class Network {
 
         if (contains(ip1) && contains(ip2)) {
 
-            if (traversal(ip1, new LinkedHashSet<>(), this).contains(ip2)) {
+            if (traversal(ip1, new LinkedHashSet<>()).contains(ip2)) {
                 return false;
             }
-            this.treeTopology.get(ip1).add(ip2);
-            this.treeTopology.get(ip2).add(ip1);
+            this.adjacencyList.get(ip1).add(ip2);
+            this.adjacencyList.get(ip2).add(ip1);
             return true;
         } else return false;
 
@@ -171,20 +169,20 @@ public class Network {
      * @return boolean if the disconnect was successful or not
      */
     public boolean disconnect(final IP ip1, final IP ip2) {
-        if (this.treeTopology.size() == 2) {
+        if (this.adjacencyList.size() == 2) {
             return false;
         }
-        if (this.treeTopology.containsKey(ip1)
-                && this.treeTopology.containsKey(ip2)
-                && this.treeTopology.get(ip1).contains(ip2)
-                && this.treeTopology.get(ip2).contains(ip1)) {
-            this.treeTopology.get(ip1).remove(ip2);
-            if (this.treeTopology.get(ip1).isEmpty()) {
-                this.treeTopology.remove(ip1);
+        if (this.adjacencyList.containsKey(ip1)
+                && this.adjacencyList.containsKey(ip2)
+                && this.adjacencyList.get(ip1).contains(ip2)
+                && this.adjacencyList.get(ip2).contains(ip1)) {
+            this.adjacencyList.get(ip1).remove(ip2);
+            if (this.adjacencyList.get(ip1).isEmpty()) {
+                this.adjacencyList.remove(ip1);
             }
-            this.treeTopology.get(ip2).remove(ip1);
-            if (this.treeTopology.get(ip2).isEmpty()) {
-                this.treeTopology.remove(ip2);
+            this.adjacencyList.get(ip2).remove(ip1);
+            if (this.adjacencyList.get(ip2).isEmpty()) {
+                this.adjacencyList.remove(ip2);
             }
             return true;
         } else {
@@ -198,7 +196,7 @@ public class Network {
      * @return if the ip input exists in the tree Topology
      */
     public boolean contains(final IP ip) {
-        return this.treeTopology.containsKey(ip);
+        return this.adjacencyList.containsKey(ip);
     }
 
     /**
@@ -210,7 +208,7 @@ public class Network {
      * @return the hight of the Tree
      */
     public int getHeight(final IP root) {
-        if (!this.treeTopology.containsKey(root)) {
+        if (!this.adjacencyList.containsKey(root)) {
             return 0;
         } else {
             return getLevels(root).size() - 1;
@@ -230,7 +228,7 @@ public class Network {
 
 
         List<List<IP>> levels = new LinkedList<>();
-        if (!this.treeTopology.containsKey(root)) {
+        if (!this.adjacencyList.containsKey(root)) {
             return levels;
         }
         levelList(root, 0, levels, null);
@@ -256,7 +254,7 @@ public class Network {
         if (start == null || end == null) {
             return route;
         }
-        if (start.equals(end) || !this.treeTopology.containsKey(start) || !this.treeTopology.containsKey(end)) {
+        if (start.equals(end) || !this.adjacencyList.containsKey(start) || !this.adjacencyList.containsKey(end)) {
             return route;
         }
         route(start, end, new LinkedHashSet<>(), route);
@@ -264,14 +262,13 @@ public class Network {
     }
 
     /**
-     * Returns the networstructure as bracketnotaion from the given IP as root.
-     *
+     * Returns the networstructure as bracket notation from the given IP as root.
      * @param root IP
      * @return Bracketnotaion as string
      */
     public String toString(IP root) {
 
-        if (root == null || !this.treeTopology.containsKey(root)) {
+        if (root == null || !this.adjacencyList.containsKey(root)) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
@@ -280,23 +277,36 @@ public class Network {
         return sb.toString();
     }
 
-    private HashSet<IP> traversal(IP root, LinkedHashSet<IP> visited, Network subnet) {
+    /**
+     * Method do travel through the Tree. (based on DFS algorithm)
+     * @param root starting point
+     * @param visited set of visited nodes
+     * @return the List of the Visited nodes
+     */
+    private HashSet<IP> traversal(IP root, LinkedHashSet<IP> visited) {
 
         visited.add(root);
 
-        for (IP ip : subnet.treeTopology.get(root)) {
+        for (IP ip : this.adjacencyList.get(root)) {
             if (!visited.contains(ip)) {
-                traversal(ip, visited, subnet);
+                traversal(ip, visited);
             }
         }
         return visited;
     }
 
+    /**
+     * Checks if the Tree contains a cycle. (based on DFS algorithm)
+     * @param root starting point
+     * @param parent parent of the node
+     * @param visited Set of visited nodes
+     * @return if there is a cycle or not
+     */
     private boolean checkCycle(IP root, IP parent, LinkedHashSet<IP> visited) {
 
         visited.add(root);
 
-        for (IP ip : treeTopology.get(root)) {
+        for (IP ip : adjacencyList.get(root)) {
             if (!ip.equals(parent) && visited.contains(ip)) {
                 return true;
             }
@@ -307,6 +317,14 @@ public class Network {
         return false;
     }
 
+    /**
+     * Checks if there is a route and saves the route in a Set. (based on DFS algorithm)
+     * @param start starting point of the route
+     * @param end end point of the route
+     * @param visited Set of visited notes
+     * @param route List of the IP´s of the route
+     * @return boolean if there is a route or not
+     */
     private boolean route(IP start, IP end, LinkedHashSet<IP> visited, LinkedList<IP> route) {
 
         visited.add(start);
@@ -315,7 +333,7 @@ public class Network {
             return true;
         }
 
-        for (IP ip : treeTopology.get(start)) {
+        for (IP ip : adjacencyList.get(start)) {
             if (!visited.contains(ip) && route(ip, end, visited, route)) {
                 return true;
             }
@@ -324,13 +342,19 @@ public class Network {
         return false;
     }
 
+    /**
+     * Builds a tree into the bracketnotation. (based on DFS algorithm)
+     * @param root root of the tree
+     * @param parent parent of the node
+     * @param result StringBuilder where the bracketnotaion is saved
+     */
     private void bracketNotation(IP root, IP parent, StringBuilder result) {
 
-        if (treeTopology.get(root).size() == 1 && parent != null) {
+        if (adjacencyList.get(root).size() == 1 && parent != null) {
             result.append(root);
         } else {
             result.append("(").append(root).append(" ");
-            for (IP ip : treeTopology.get(root)) {
+            for (IP ip : adjacencyList.get(root)) {
                 if (!ip.equals(parent)) {
                     bracketNotation(ip, root, result);
                     result.append(" ");
@@ -341,19 +365,33 @@ public class Network {
         }
     }
 
+    /**
+     * Puts all Levels of the Tree into a list. (based on DFS algorithm)
+     * @param root starting point
+     * @param level current level
+     * @param list list where the different levels are saved
+     * @param parent parent of the node
+     */
     private void levelList(IP root, int level, List<List<IP>> list, IP parent) {
 
         if (list.size() == level) {
             list.add(new LinkedList<>());
         }
         list.get(level).add(root);
-        for (IP ip : treeTopology.get(root)) {
+        for (IP ip : adjacencyList.get(root)) {
             if (!ip.equals(parent)) {
                 levelList(ip, level + 1, list, root);
             }
         }
     }
 
+    /**
+     * Checks if the Input was correct.
+     * @param root root ip
+     * @param children List of Children IP´s
+     * @return the children as TreeSet
+     * @throws IllegalArgumentException if the Input was not correct
+     */
     private TreeSet<IP> constructorInputValidation(IP root, List<IP> children) throws IllegalArgumentException {
 
         if (children == null) {
@@ -379,6 +417,11 @@ public class Network {
 
     }
 
+    /**
+     * Checks if the Input was correct.
+     * @param bracketNotation a Tree in bracket notation
+     * @throws ParseException if the input was not correct
+     */
     private void constructorInputValidation(String bracketNotation) throws ParseException {
 
         if (bracketNotation == null) {
@@ -403,43 +446,52 @@ public class Network {
         }
     }
 
-    private void convertIPToAdjList(IP newIp, LinkedList<IP> rootList) throws ParseException {
+    /**
+     * Adds a new IP and connects it with the last element of the root List and adds it to the adjacency list.
+     * @param ip new node
+     * @param rootList list of roots (root of every subtree)
+     * @throws ParseException if the IP already exists
+     */
+    private void addIP(IP ip, LinkedList<IP> rootList) throws ParseException {
 
-        if (treeTopology.containsKey(newIp)) {
+        if (adjacencyList.containsKey(ip)) {
             throw new ParseException(ILLEGAL_TREE_TOPOLOGY);
         }
-        treeTopology.put(newIp, new TreeSet<>());
-        connect(rootList.getLast(), newIp);
+        adjacencyList.put(ip, new TreeSet<>());
+        connect(rootList.getLast(), ip);
     }
 
+    /**
+     * Checks if the subnet is null or a part tree
+     * @param subnet Network to check
+     * @return if the Validation was true
+     */
     private boolean addValidation(Network subnet) {
 
         if (subnet == null) {
             return false;
         }
-        for (IP ip: subnet.treeTopology.keySet()) {
-            if (!this.treeTopology.containsKey(ip)) {
+        for (IP ip: subnet.adjacencyList.keySet()) {
+            if (!this.adjacencyList.containsKey(ip)) {
                 return true;
             }
-            if (!this.treeTopology.get(ip).containsAll(subnet.treeTopology.get(ip))) {
+            if (!this.adjacencyList.get(ip).containsAll(subnet.adjacencyList.get(ip))) {
                 return true;
             }
-
         }
         return false;
     }
-
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Network network = (Network) o;
-        return Objects.equals(treeTopology, network.treeTopology);
+        return Objects.equals(adjacencyList, network.adjacencyList);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(treeTopology);
+        return Objects.hash(adjacencyList);
     }
 }
